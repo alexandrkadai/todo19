bfd2f2fef568b202093853141d50940f1916f618
Alex Kadai <alexandrkadai@gmail.com>
2024-12-30 15:00:50 +0200

    Prettier

--- src/pages/users/page.tsx
+++ src/pages/users/page.tsx
@@ -10,14 +10,16 @@ export function UserPage() {
 
   return (
     <>
-      <h1 className="">Users</h1>
-      <section className="">
-        <form action="" className="">
-
-        </form>
+      <h1 className="flex items-center justify-center text-2xl font-bold">
+        Users
+      </h1>
+      <section className="mt-2">
+        <form action="" className=""></form>
       </section>
       <section className="">
-        <h2 className="text-2xl">Users List</h2>
+        <h2 className="flex items-center justify-center text-xl font-bold">
+          Users List
+        </h2>
         <ul>
           {users.map((user) => (
             <li>{user.email}</li>
